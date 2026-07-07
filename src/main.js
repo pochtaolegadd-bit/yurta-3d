c1c342a514182cf65c9dd1cf00463e1b3f21bc6f
pochtaolegadd-bit <pochtaolegadd@gmail.com>
2026-07-07 21:30:24 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -444,3 +444,379 @@ let cameraTouchId = null;
 
 let lastTouchX = 0;
 let lastTouchY = 0;
+if(isMobile){
+
+
+// =======================
+// ПОВОРОТ КАМЕРЫ ВТОРЫМ ПАЛЬЦЕМ
+// =======================
+
+
+document.addEventListener(
+"touchstart",
+(e)=>{
+
+
+for(const touch of e.changedTouches){
+
+
+if(
+touch.target.classList.contains(
+"mobileControl"
+)
+)
+continue;
+
+
+
+if(cameraTouchId === null){
+
+cameraTouchId =
+touch.identifier;
+
+
+lastTouchX =
+touch.clientX;
+
+
+lastTouchY =
+touch.clientY;
+
+}
+
+}
+
+
+});
+
+
+
+
+
+document.addEventListener(
+"touchmove",
+(e)=>{
+
+
+for(const touch of e.changedTouches){
+
+
+if(
+touch.identifier !== cameraTouchId
+)
+continue;
+
+
+let dx =
+touch.clientX - lastTouchX;
+
+
+let dy =
+touch.clientY - lastTouchY;
+
+
+
+yaw -= dx * 0.005;
+
+pitch -= dy * 0.005;
+
+
+
+pitch=Math.max(
+-Math.PI/2,
+Math.min(Math.PI/2,pitch)
+);
+
+
+
+camera.rotation.order="YXZ";
+
+camera.rotation.y=yaw;
+
+camera.rotation.x=pitch;
+
+
+
+lastTouchX =
+touch.clientX;
+
+
+lastTouchY =
+touch.clientY;
+
+
+}
+
+
+});
+
+
+document.addEventListener(
+"touchend",
+(e)=>{
+
+for(const touch of e.changedTouches){
+
+if(
+touch.identifier === cameraTouchId
+){
+
+cameraTouchId=null;
+
+}
+
+}
+
+});
+
+
+
+
+
+// =======================
+// ДЖОЙСТИК
+// =======================
+
+
+const base =
+document.createElement("div");
+
+
+base.className =
+"mobileControl";
+
+
+Object.assign(
+base.style,
+{
+position:"fixed",
+left:"40px",
+bottom:"40px",
+width:"130px",
+height:"130px",
+borderRadius:"50%",
+background:"rgba(255,255,255,0.25)",
+zIndex:"9999",
+touchAction:"none"
+}
+);
+
+
+document.body.appendChild(base);
+
+
+
+
+const stick =
+document.createElement("div");
+
+
+stick.className =
+"mobileControl";
+
+
+Object.assign(
+stick.style,
+{
+position:"absolute",
+left:"40px",
+top:"40px",
+width:"50px",
+height:"50px",
+borderRadius:"50%",
+background:"white"
+}
+);
+
+
+base.appendChild(stick);
+
+
+
+
+base.addEventListener(
+"touchmove",
+(e)=>{
+
+
+e.preventDefault();
+
+
+e.stopPropagation();
+
+
+
+const touch =
+e.touches[0];
+
+
+const rect =
+base.getBoundingClientRect();
+
+
+
+let x =
+touch.clientX -
+(rect.left+65);
+
+
+let y =
+touch.clientY -
+(rect.top+65);
+
+
+
+x=Math.max(
+-45,
+Math.min(45,x)
+);
+
+
+y=Math.max(
+-45,
+Math.min(45,y)
+);
+
+
+
+stick.style.left =
+40+x+"px";
+
+
+stick.style.top =
+40+y+"px";
+
+
+
+joystickX =
+x/45;
+
+
+joystickY =
+y/45;
+
+
+},
+{
+passive:false
+});
+
+
+
+base.addEventListener(
+"touchend",
+()=>{
+
+joystickX=0;
+joystickY=0;
+
+
+stick.style.left="40px";
+stick.style.top="40px";
+
+});
+
+
+
+
+
+// =======================
+// КНОПКИ ВВЕРХ ВНИЗ
+// =======================
+
+
+function button(text,bottom){
+
+
+let b =
+document.createElement("button");
+
+
+b.className =
+"mobileControl";
+
+
+b.innerHTML=text;
+
+
+
+Object.assign(
+b.style,
+{
+position:"fixed",
+right:"40px",
+bottom:bottom,
+width:"75px",
+height:"75px",
+borderRadius:"50%",
+fontSize:"35px",
+zIndex:"9999",
+userSelect:"none",
+touchAction:"none"
+}
+);
+
+
+
+document.body.appendChild(b);
+
+
+return b;
+
+}
+
+
+
+const up =
+button("⬆️","140px");
+
+
+const down =
+button("⬇️","50px");
+
+
+
+up.addEventListener(
+"touchstart",
+(e)=>{
+
+e.stopPropagation();
+
+verticalMove=1;
+
+});
+
+
+
+up.addEventListener(
+"touchend",
+()=>{
+
+verticalMove=0;
+
+});
+
+
+
+down.addEventListener(
+"touchstart",
+(e)=>{
+
+e.stopPropagation();
+
+verticalMove=-1;
+
+});
+
+
+
+down.addEventListener(
+"touchend",
+()=>{
+
+verticalMove=0;
+
+});
+
+
+}
